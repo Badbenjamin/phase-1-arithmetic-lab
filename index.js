ca496bfed936ef8617d4780e89e224fee70b045c
ben mccarthy <bmccarthy90@gmail.com>
2024-07-12 18:28:10 -0400

    passing arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let random = Math.round(Math.random() * 1000);
+
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+let num3 = 16;
+let num4 = 6;
+
+let mod = (num3 % num4);
+
+
+let max = Math.max(1, 2, 3, 4, 20);
